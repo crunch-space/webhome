1889787be94abc56908af853edf6341ba082c41c
李淏 <3441566666@qq.com>
2024-07-16 14:48:34 +0800

    web

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <!-- <Nav/> -->
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// import Nav from './view/components/navbar.vue'
+export default {
+    components: {
+    // Foot,
+    // Nav,
+    //  Foots
+  },
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+*{
+  margin: 0;
+  padding: 0;
+ 
+}
+html {
+  overflow-x: hidden;
+  -ms-overflow-style: none; 
+  scrollbar-width: none; 
+   background: black;
+}
+html::-webkit-scrollbar {
+  display: none; 
+}
+</style>
